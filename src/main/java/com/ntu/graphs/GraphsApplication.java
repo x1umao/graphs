0b59630000e871d76fe4a06736c5d487257467a1
x1umao <981410929@qq.com>
2021-04-20 03:55:34 +0800

    modify the href of logo.

--- src/main/java/com/ntu/graphs/GraphsApplication.java
+++ src/main/java/com/ntu/graphs/GraphsApplication.java
@@ -4,9 +4,14 @@ import com.ntu.graphs.dao.AdminRepository;
 import com.ntu.graphs.dao.ArticleRepository;
 import com.ntu.graphs.dao.PersonRepository;
 import com.ntu.graphs.entity.Admin;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
