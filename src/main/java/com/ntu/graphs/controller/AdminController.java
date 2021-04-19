ae57b08b7e6a15a9b01eff7191d75b36f2133f25
x1umao <981410929@qq.com>
2021-04-20 04:16:19 +0800

    add counter of nodes at admin-dash

--- src/main/java/com/ntu/graphs/controller/AdminController.java
+++ src/main/java/com/ntu/graphs/controller/AdminController.java
@@ -1,20 +1,36 @@
 package com.ntu.graphs.controller;
 
 import com.ntu.graphs.entity.User;
+import com.ntu.graphs.service.ArticleService;
+import com.ntu.graphs.service.JournalService;
+import com.ntu.graphs.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AdminController {
 
+    private final ArticleService articleService;
+    private final PersonService personService;
+    private final JournalService journalService;
+
+    public AdminController(ArticleService articleService, PersonService personService, JournalService journalService) {
+        this.articleService = articleService;
+        this.personService = personService;
+        this.journalService = journalService;
+    }
+
     @GetMapping("/admin-dash")
     public String adminDash(Model model, HttpServletRequest request){
         User user = (User) request.getSession().getAttribute("user");
         model.addAttribute("username",user.getUsername());
+        model.addAttribute("person", personService.countNodes());
+        model.addAttribute("article", articleService.countNodes());
+        model.addAttribute("journal", journalService.countNodes());
         return "admin-dash";
     }
 
